26de4e570d219d775515c4eacc3554f0b489f746
Roman Isaikin <romix.lab@gmail.com>
2018-02-20 16:34:01 +0300

    Fix crc calculation. Add ev2300 submit command, presumably. Fix example.

--- ev2300.py
+++ ev2300.py
@@ -16,22 +16,23 @@ chromalog.basicConfig(format="%(message)s", level=logging.DEBUG)
 logger = logging.getLogger()
 
 class EV2300:
-	READ_WORD   = 0x01
-	WRITE_WORD  = 0x04
-	READ_BLOCK  = 0x02
-	WRITE_BLOCK = 0x05
-	COMMAND	    = 0x06
-	ERROR_CODE  = 0x46
+	READ_WORD    = 0x01
+	WRITE_WORD   = 0x04
+	READ_BLOCK   = 0x02
+	WRITE_BLOCK  = 0x05
+	COMMAND	     = 0x06
+	ERROR_CODE   = 0x46
+	MAYBE_SUBMIT = 0x80
 
 	def __init__(self):
 		self.dev = None
 		self.firmwareFilename = "ev2300_default.bin"
 		self.buffer = array('B', [0]) * 64
 
-	def __load_firmware(self, dev):
-		logger.info("Loading firmware: %s", important(EV2300_FIRMWARE))
+	def _load_firmware(self, dev):
+		logger.info("Loading firmware: %s", important(self.firmwareFilename))
 		firmware = array('B')
-		with open(EV2300_FIRMWARE, 'rb') as f:
+		with open(self.firmwareFilename, 'rb') as f:
 			firmware.fromfile(f, 8073)
 		header = array('B', b'\x89\x1f\x2b') # length_l, length_h, ?
 		try:
@@ -43,7 +44,7 @@ class EV2300:
 			logger.error("Firmware loading error: " + str(e))
 		return True
 
-	def __prepare(self):
+	def _prepare(self):
 		dev = usb.core.find(idVendor=0x0451, idProduct=0x0036) # flashed
 		if dev is None:
 			dev = usb.core.find(idVendor=0x0451, idProduct=0x2136) # empty
@@ -51,7 +52,7 @@ class EV2300:
 				logger.warning('EV2300 not connected, waiting...')
 			else:
 				dev.set_configuration()
-				if load_firmware(dev):
+				if self._load_firmware(dev):
 					logger.info('Firmware loaded, waiting...')
 					timeout = 5.0 #s
 					while timeout > 0:
@@ -74,7 +75,7 @@ class EV2300:
 		time_left = timeout
 		while True:
 			try:
-				self.dev = self.__prepare()
+				self.dev = self._prepare()
 			except usb.core.USBError as e:
 				if e.errno == 13:
 					logger.warning('Access denied, configure udev or run as root')
@@ -98,40 +99,54 @@ class EV2300:
 			logger.info("EV2300: %s", success("READY"))
 			return True
 
-	def __check_state(self):
+	def _check_state(self):
 		if self.dev == None:
 			logger.warning('EV2300 not ready, call prepare() first')
 			raise AssertionError('EV2300 not ready')
 
-	def __check_arguments(self, device_address, register_address):
+	def _check_arguments(self, device_address, register_address):
 		if not (0 <= device_address <= 127):
 			raise ValueError('Device address is invalid, should be (0-127)', device_address)
 		if not (0 <= register_address <= 255):
 			raise ValueError('Register address is invalid, should be (0-255)', register_address)
 
-	def __calculate_crc(self, slice):
+	def _calculate_crc(self, slice):
 		crc = crc8.crc8()
 		crc.update(slice.tostring())
 		digest = array('B')
 		digest.fromstring(crc.digest())
 		return digest[0]
 
-	def __fill_with_zeroes(self, after):
+	def _fill_with_zeroes(self, after):
 		self.buffer[after+1:64] = array('B', [0]) * (64 - after - 1)
 
-	def __request(self):
+	def _prepare_submit(self):
+		self.buffer[0]    = 8
+		self.buffer[1]    = 0xaa;
+		self.buffer[2]    = self.MAYBE_SUBMIT
+		self.buffer[3:6]  = array('B', [0]) * 3
+		self.buffer[6]    = 0 # bytes till crc
+		self.buffer[7]   = self._calculate_crc(self.buffer[2:7])
+		self.buffer[8]   = 0x55
+		self._fill_with_zeroes(self.buffer[0])
+
+	def _request(self, timeout = 100, write_submit = False):
 		try:
-			self.dev.write(0x1, self.buffer)
-			self.response = self.dev.read(0x81, 64, 1000)
+			self.dev.write(0x1, self.buffer, timeout)
+			if write_submit:
+				self._prepare_submit()
+				self.dev.write(0x1, self.buffer, timeout)
+			self.response = self.dev.read(0x81, 64, timeout)
 			if len(self.response) != 64:
 				logger.warning('Readed back length != 64', len(self.response))
 				return False
 			if self.response[2] == self.ERROR_CODE:
 				logger.warning('Request error', format('{:02x}', self.response[2]))
+				return False
 			else:
 				length = self.response[0]
 				received_crc = self.response[length - 1]
-				crc = self.__calculate_crc(self.response[2:length-1])
+				crc = self._calculate_crc(self.response[2:length-1])
 				if crc != received_crc:
 					logger.warning('Received message CRC mismatch')
 					return False
@@ -141,8 +156,8 @@ class EV2300:
 			return False
 
 	def smbus_read_word(self, device_address, register_address):
-		self.__check_state()
-		self.__check_arguments(device_address, register_address)
+		self._check_state()
+		self._check_arguments(device_address, register_address)
 
 		self.buffer[0]   = 10 # total bytes except first
 		self.buffer[1]   = 0xaa;
@@ -151,17 +166,17 @@ class EV2300:
 		self.buffer[6]   = 2 # bytes till crc
 		self.buffer[7]   = device_address << 1
 		self.buffer[8]   = register_address
-		self.buffer[9]   = self.__calculate_crc(self.buffer[2:9])
+		self.buffer[9]   = self._calculate_crc(self.buffer[2:9])
 		self.buffer[10]  = 0x55
-		self.__fill_with_zeroes(self.buffer[0]) # 11-63 bytes
+		self._fill_with_zeroes(self.buffer[0]) # 11-63 bytes
 
-		if not self.__request():
+		if not self._request():
 			return None
 		return struct.unpack('<H', self.response[8:10])[0]
 
 	def smbus_write_word(self, device_address, register_address, word):
-		self.__check_state()
-		self.__check_arguments(device_address, register_address)
+		self._check_state()
+		self._check_arguments(device_address, register_address)
 		if not (0 <= word <= 0xffff):
 			raise ValueError('word not in range 0-ffff', word)
 
@@ -173,15 +188,15 @@ class EV2300:
 		self.buffer[7]    = device_address << 1
 		self.buffer[8]    = register_address
 		self.buffer[9:11] = array('B', struct.pack('<H', word))
-		self.buffer[11]   = self.__calculate_crc(self.buffer[2:9])
+		self.buffer[11]   = self._calculate_crc(self.buffer[2:11])
 		self.buffer[12]   = 0x55
-		self.__fill_with_zeroes(self.buffer[0]) # 11-63 bytes
+		self._fill_with_zeroes(self.buffer[0])
 
-		return self.__request()
+		return self._request(write_submit = True)
 
 	def smbus_read_block(self, device_address, block_address):
-		self.__check_state()
-		self.__check_arguments(device_address, block_address)
+		self._check_state()
+		self._check_arguments(device_address, block_address)
 
 		self.buffer[0]    = 10 # total bytes except first
 		self.buffer[1]    = 0xaa;
@@ -190,18 +205,18 @@ class EV2300:
 		self.buffer[6]    = 2 # bytes till crc
 		self.buffer[7]    = device_address << 1
 		self.buffer[8]    = block_address
-		self.buffer[9]   = self.__calculate_crc(self.buffer[2:9])
+		self.buffer[9]   = self._calculate_crc(self.buffer[2:9])
 		self.buffer[10]   = 0x55
-		self.__fill_with_zeroes(self.buffer[0]) # 11-63 bytes
+		self._fill_with_zeroes(self.buffer[0])
 
-		if not self.__request():
+		if not self._request():
 			return None
 		block_length = self.response[8]
 		return self.response[9:9+block_length]
 
 	def smbus_write_block(self, device_address, block_address, block):
-		self.__check_state()
-		self.__check_arguments(device_address, block_address)
+		self._check_state()
+		self._check_arguments(device_address, block_address)
 		if not (0 <= len(block) <= 52):
 			raise ValueError('Block length not in range (0-52)', len(block))
 
@@ -214,14 +229,14 @@ class EV2300:
 		self.buffer[8]    = block_address
 		self.buffer[9]    = len(block)
 		self.buffer[10:10+len(block)] = block
-		self.buffer[10+len(block)]    = self.__calculate_crc(self.buffer[2:9])
+		self.buffer[10+len(block)]    = self._calculate_crc(self.buffer[2:10+len(block)])
 		self.buffer[10+len(block)+1]    = 0x55
-		self.__fill_with_zeroes(self.buffer[0]) # 11-63 bytes
+		self._fill_with_zeroes(self.buffer[0])
 
-		return self.__request()
+		return self._request(write_submit = True)
 
 	def smbus_command(self, device_address, command):
-		self.__check_state()
+		self._check_state()
 		if not (0 <= command <= 0xff):
 			raise ValueError('Command number not in range 0-255', command)
 
@@ -232,8 +247,8 @@ class EV2300:
 		self.buffer[6]    = 2 # bytes till crc
 		self.buffer[7]    = device_address << 1
 		self.buffer[8]    = command
-		self.buffer[9]    = self.__calculate_crc(self.buffer[2:9])
+		self.buffer[9]    = self._calculate_crc(self.buffer[2:9])
 		self.buffer[10]   = 0x55
-		self.__fill_with_zeroes(self.buffer[0]) # 11-63 bytes
+		self._fill_with_zeroes(self.buffer[0])
 
-		return self.__request()
+		return self._request(write_submit = True)
--- example.py
+++ example.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import ev2300
+from array import array
+
+import time
 
 ev = ev2300.EV2300()
 ev.prepare()
 
-print(ev.smbus_read_word(0x40, 0x01))
 print(ev.smbus_write_word(0x40, 0x01, 0))
+print(ev.smbus_read_word(0x40, 0x01))
 
 block = ev.smbus_read_block(0x40, 0x99)
 print(''.join('{:c} '.format(x) for x in block))
 
-print(ev.smbus_write_block(0x40, 0x9d, b'\x01\x02\x03\x04\x05\x06'))
+print(ev.smbus_write_block(0x40, 0x9d, array('B', b'\x01\x02\x03\x04\x05\x06'))
 
 print(ev.smbus_command(0x40, 0x02))
